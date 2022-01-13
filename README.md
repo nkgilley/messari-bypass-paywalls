24c6c68e0877b25509b0c506f8c3cc8b636270f0
Nolan Gilley <nkgilley@gmail.com>
2022-01-13 10:23:44 -0500

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to install
+1. Download the zip file of this repo. (Code > Download ZIP)
+2. Extract the zip file
+3. Open the extensions page: [chrome://extensions](chrome://extensions)
+4. Enable developer mode toggle switch in the top right of the extensions page
+5. Click the `Load Unpacked` button and select the extracted folder from step 2
--- content-script.js
+++ content-script.js
@@ -1,4 +1,28 @@
 window.addEventListener("load", updatePageContents, false);
+window.addEventListener("load", watchForRedirect, false);
+
+
+function watchForRedirect() {
+
+    // Select the node that will be observed for mutations
+    const targetNode = document.querySelector('div#root');
+
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true };
+
+    // Callback function to execute when mutations are observed
+    const callback = function(mutationsList, observer) {
+        for(const mutation of mutationsList) {
+            updatePageContents();
+        }
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+}
 
 function updatePageContents() {
   
@@ -6,22 +30,23 @@ function updatePageContents() {
     let paywall = document.querySelector("div.Messari-paywall")
     if (!paywall) return
 
+    // clone existing content and remove all classes
     let newContent = paywall.cloneNode(true)
-    // strip classes (not sure why need to repeat, but do need to)
-    for (let i=0; i <= newContent.classList.length; i++) {
-        newContent.classList.remove(newContent.classList[i])
-    }
-    for (let i=0; i <= newContent.classList.length; i++) {
-        newContent.classList.remove(newContent.classList[i])
+    newContent.removeAttribute('class')
+    newContent.style.cssText = 'text-align: left'
+    newContent.children[0].removeAttribute('class')
+    let links = newContent.getElementsByTagName('a')
+    for (let i = 0; i < links.length; i++) {
+        links[i].style.color = '#0091EA'
     }
 
     // remove old paywall content
     paywall.remove()
 
-    // find where to insert copied content
+    // find where to insert cloned content
     let h2 = document.querySelector('h2')
 
-    // insert copied content
+    // insert cloned content
     h2.parentElement.insertBefore(newContent, h2)
     
 }
